4a582937a35a35f5bfd0db3d86d859c331a10644
samuel-looper <samuel.looper@mail.utoronto.ca>
2021-03-26 10:30:26 -0400

    End2End New Architecture Test

--- End2EndNet.py
+++ End2EndNet.py
@@ -6,8 +6,6 @@ from data_loader import TrainSet
 from torch.utils.data import DataLoader
 import math
 
-PATH = 'E2E_v5.pth'
-
 
 class Chomp1d(nn.Module):
     def __init__(self, chomp_size):
@@ -67,73 +65,104 @@ class E2ESingleStepTCN(nn.Module):
         K = 8
         d = 2
         self.L = L
-        self.tconv1 = TConvBlock(L+P, 16, 16, K, d)
+        self.tconv1 = TConvBlock(L + P, 16, 16, K, d)
         self.bn1 = torch.nn.BatchNorm1d(16)
         self.relu1 = torch.nn.ReLU()
         self.tconv2 = TConvBlock(L + P, 16, 16, K, d)
         self.bn2 = torch.nn.BatchNorm1d(16)
         self.relu2 = torch.nn.ReLU()
-        self.tconv3 = TConvBlock(L + P, 16, 32, K, d)
+        self.tconv2 = TConvBlock(L + P, 16, 16, K, d)
+        self.bn2 = torch.nn.BatchNorm1d(16)
+        self.relu2 = torch.nn.ReLU()
+        self.tconv3 = TConvBlock(P + int(L / 2), 16, 32, K, d)
         self.bn3 = torch.nn.BatchNorm1d(32)
         self.relu3 = torch.nn.ReLU()
-        self.tconv4 = TConvBlock(P + int(L/2), 32, 32, K, d)
+        self.tconv4 = TConvBlock(P + int(L / 2), 32, 32, K, d)
         self.bn4 = torch.nn.BatchNorm1d(32)
         self.relu4 = torch.nn.ReLU()
-        self.tconv5 = TConvBlock(P + int(L/2), 32, 64, K, d)
+        self.tconv5 = TConvBlock(P, 32, 64, K, d)
         self.bn5 = torch.nn.BatchNorm1d(64)
         self.relu5 = torch.nn.ReLU()
-        self.tconv6 = TConvBlock(P + int(L/2), 64, 64, K, d)
+        self.tconv6 = TConvBlock(P, 64, 64, K, d)
         self.bn6 = torch.nn.BatchNorm1d(64)
         self.relu6 = torch.nn.ReLU()
         self.tconv7 = TConvBlock(P, 64, 128, K, d)
         self.bn7 = torch.nn.BatchNorm1d(128)
         self.relu7 = torch.nn.ReLU()
-        self.tconv8 = TConvBlock(P, 128, 128, K, d)
-        self.bn8 = torch.nn.BatchNorm1d(128)
-        self.relu8 = torch.nn.ReLU()
-        self.tconv9 = TConvBlock(P, 128, 256, K, d)
-        self.bn9 = torch.nn.BatchNorm1d(256)
-        self.relu9 = torch.nn.ReLU()
-        self.tconv10 = TConvBlock(P, 256, 6, K, d)
+        self.tconv8 = TConvBlock(P, 128, 6, K, d)
 
     def forward(self, input):
         # Assume X: batch by length by channel size
         # print(input.shape)
         x1 = self.relu1(self.bn1(self.tconv1(input)))
         x2 = x1 + self.relu2(self.bn2(self.tconv2(x1)))
-        x3 = self.relu3(self.bn3(self.tconv3(x2)))
-        x4 = x3[:, :, int(self.L/2):] + self.relu4(self.bn4(self.tconv4(x3[:, :, int(self.L/2):])))
-        x5 = self.relu5(self.bn5(self.tconv5(x4)))
+        x3 = self.relu3(self.bn3(self.tconv3(x2[:, :, int(self.L / 2):])))
+        x4 = x3 + self.relu4(self.bn4(self.tconv4(x3)))
+        x5 = self.relu5(self.bn5(self.tconv5(x4[:, :, int(self.L / 2):])))
         x6 = x5 + self.relu6(self.bn6(self.tconv6(x5)))
-        x7 = self.relu7(self.bn7(self.tconv7(x6[:, :, int(self.L/2):])))
-        x8 = x7 + self.relu8(self.bn8(self.tconv8(x7)))
-        x9 = self.relu9(self.bn9(self.tconv9(x8)))
-        x10 = self.tconv10(x9)
+        x7 = self.relu7(self.bn7(self.tconv7(x6)))
+        x8 = self.tconv8(x7)
         # print(x.shape)
-        return x10
+        return x8
 
 
-def train_model():
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    torch.set_default_tensor_type("torch.cuda.FloatTensor")
+class E2ESingleStepTCN_NewArch(nn.Module):
+    def __init__(self, lookback, forward_step):
+        super(E2ESingleStepTCN_NewArch, self).__init__()
+        L = lookback
+        P = forward_step
+        K = 8
+        d = 2
+        self.L = L
+        self.tconv1 = TConvBlock(L-1, 16, 16, K, d)
+        self.bn1 = torch.nn.BatchNorm1d(16)
+        self.relu1 = torch.nn.ReLU()
+        self.tconv2 = TConvBlock(L-1, 16, 16, K, d)
+        self.bn2 = torch.nn.BatchNorm1d(16)
+        self.relu2 = torch.nn.ReLU()
+        self.tconv3 = TConvBlock(L-1, 16, 16, K, d)
+        self.bn3 = torch.nn.BatchNorm1d(16)
+        self.relu3 = torch.nn.ReLU()
+        self.tconv4 = TConvBlock(L-1, 16, 16, K, d)
+        self.bn4 = torch.nn.BatchNorm1d(16)
+        self.relu4 = torch.nn.ReLU()
+        self.tconv5 = TConvBlock(L+P, 16, 32, K, d)
+        self.bn5 = torch.nn.BatchNorm1d(32)
+        self.relu5 = torch.nn.ReLU()
+        self.tconv6 = TConvBlock(L+P, 32, 64, K, d)
+        self.bn6 = torch.nn.BatchNorm1d(64)
+        self.relu6 = torch.nn.ReLU()
+        self.tconv7 = TConvBlock(P + int(L / 2), 64, 64, K, d)
+        self.bn7 = torch.nn.BatchNorm1d(64)
+        self.relu7 = torch.nn.ReLU()
+        self.tconv8 = TConvBlock(P, 64, 128, K, d)
+        self.bn8 = torch.nn.BatchNorm1d(128)
+        self.relu8 = torch.nn.ReLU()
+        self.tconv9 = TConvBlock(P, 128, 6, K, d)
 
-    lr = 0.0001
-    wd = 0.00005
-    epochs = 100
-    bs = 16
-    L =64
-    P = 60
-    tv_set = TrainSet('data/AscTec_Pelican_Flight_Dataset.mat', L, P, full_set=True)
+    def forward(self, input):
+        # Assume X: batch by length by channel size
+        # print(input.shape)
+        in1 = input[:, :, self.L-1:]
 
-    train_len = int(len(tv_set) * 0.8)
-    val_len = len(tv_set) - train_len
-    train_set, val_set = torch.utils.data.random_split(tv_set, [train_len, val_len], torch.Generator())
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=bs, shuffle=True, num_workers=0)
-    val_loader = torch.utils.data.DataLoader(val_set, batch_size=bs, shuffle=True, num_workers=0)
-    print("Data Loaded Successfully")
+        x1 = self.relu1(self.bn1(self.tconv1(in1)))
+        x2 = self.relu2(self.bn2(self.tconv2(x1)))
+        x3 = self.relu3(self.bn3(self.tconv3(x2)))
+        x4 = self.relu4(self.bn4(self.tconv4(x3)))
 
-    net = E2ESingleStepTCN(L, P).to(device)
-    torchsummary.summary(net, (16, 124))
+        in2 = torch.cat((x4, input[:, :, :self.L-1]), 2)
+
+        x5 = self.relu5(self.bn5(self.tconv5(in2)))
+        x6 = self.relu6(self.bn6(self.tconv6(x5)))
+
+        x7 = self.relu7(self.bn7(self.tconv7(x6[:, :, int(self.L / 2):])))
+        x8 = self.relu8(self.bn8(self.tconv8(x7[:, :, int(self.L / 2):])))
+
+        x9 = self.tconv9(x8)
+        return x9
+
+
+def train_model(net, train_loader, val_loader, device, bs, epochs, lr, wd, train_len, val_len, name):
 
     loss = torch.nn.MSELoss()  # Define Mean Square Error Loss
     optimizer = torch.optim.Adam(list(net.parameters()), lr=lr, weight_decay=wd)  # Define Adam optimization algorithm
@@ -206,7 +235,7 @@ def train_model():
             if best_epoch == 0 or epoch_val_loss < best_loss:
                 best_loss = epoch_val_loss
                 best_epoch = epoch
-                torch.save(net.state_dict(), PATH)
+                torch.save(net.state_dict(), "{}.pth".format(name))
 
             # Plotting
 
@@ -227,9 +256,31 @@ def train_model():
     plt.xlabel("Epoch")
     plt.ylabel("MSE Loss")
     plt.legend(["Training Loss", "Validation Loss"])
-    plt.savefig("E2E_v5_train.png")
+    plt.savefig("{}.png".format(name))
     plt.show()
 
 
 if __name__ == "__main__":
-    train_model()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # torch.set_default_tensor_type("torch.cuda.FloatTensor")
+
+    lr = 0.0001
+    wd = 0.00005
+    epochs = 100
+    bs = 16
+    L = 64
+    P = 60
+    tv_set = TrainSet('data/AscTec_Pelican_Flight_Dataset.mat', L, P, full_set=True)
+
+    train_len = int(len(tv_set) * 0.8)
+    val_len = len(tv_set) - train_len
+    train_set, val_set = torch.utils.data.random_split(tv_set, [train_len, val_len], torch.Generator())
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=bs, shuffle=True, num_workers=0)
+    val_loader = torch.utils.data.DataLoader(val_set, batch_size=bs, shuffle=True, num_workers=0)
+    print("Data Loaded Successfully")
+
+    net = E2ESingleStepTCN(L, P).to(device)
+    train_model(net, train_loader, val_loader, device, bs, epochs, lr, wd, train_len, val_len, "End2End_Original")
+
+    net = E2ESingleStepTCN_NewArch(L, P).to(device)
+    train_model(net, train_loader, val_loader, device, bs, epochs, lr, wd, train_len, val_len, "End2End_NewArch")
